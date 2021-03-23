6ca5efd87002b40de03da6aa4d59252194342d8f
samxoxoxo <rahibsam2@gmail.com>
2021-03-23 18:25:52 +0530

    added animation

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 function text_anime() {
 
   var textWrapper = document.querySelector('.ml10 .letters');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-  anime.timeline({
-          loop: false
-      })
-      .add({
-          targets: '.ml10 .letter',
-          rotateY: [-90, 0],
-          duration: 1300,
-          delay: (el, i) => 45 * i
-      }).add({
-          targets: '.ml10',
-          duration: 1000,
-          easing: "easeOutExpo",
-          delay: 1000
-      });
+  textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+  
+  anime.timeline({loop: false})
+    .add({
+      targets: '.ml10 .letter',
+      scale: [0, 1],
+      duration: 1500,
+      elasticity: 600,
+      delay: (el, i) => 45 * (i+1)
+    }).add({
+      targets: '.ml10',
+      duration: 1000,
+      easing: "easeOutExpo",
+      delay: 1000
+    });
 }
 
 
